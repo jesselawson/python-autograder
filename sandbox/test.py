390d0b63d167962552bcaf1237dfcd3e46bf3879
Jesse Lawson <jesselawson@protonmail.com>
2020-03-08 10:06:40 -0700

    remove sandbox folder

--- sandbox/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-# How to handle two students with the same name? Need their student ID...
-
-student_test_filename = "student_test.py"
-student_results_filename = "lawsonjesse_funwithfunctions_results.txt"
-subprocess.call(f"pytest -q --tb=line {student_test_filename}  > {student_results_filename}", shell=True)

31aae57746723106bf2c6e4a949b1944f5ce0e74
Jesse Lawson <jesselawson@protonmail.com>
2020-03-19 15:51:06 -0700

    finish m12 assignment

--- assignments/m12-networkintel1/assignment_test_suite.py
+++ assignments/m12-networkintel1/assignment_test_suite.py
@@ -1,107 +1,112 @@
+import pytest
 
 # Problem 1
-problem1_target = "update_username()"
+problem1_target = "Shark('test', 5)"
 
-def test_update_username_0():
+def test_shark_class_contains_health_variable():
 
-    example = [
-        { "username": "jesse", "password": None},
-        { "username": "morty", "password": "ooh wee"}
-    ]
+    test = Shark("Testman", 5)
+
+    # Check if self.health was created
+    try:
+        test.health
+    except AttributeError:
+        return jel_assert(
+            problem1_target,
+            False,
+            f"do anything because no class instance variable named `health` was found"
+        )
 
     try:
         return jel_assert(
                 problem1_target, 
-                update_username(example, "jesse", "misterpants") == True,
-                f"return True when the username was changed"
+                test.health == 5,
+                f"set class instance variable `health` to 5"
             )
     except NameError as e:
         return jel_NameError(problem1_target, e)
     except TypeError as e:
         return jel_TypeError(problem1_target, e)
 
+test2_target = "get_health()"
 
-# Problem 1.3
-def test_update_username_2():
-
-    example = [
-        { "username": "jesse", "password": None},
-        { "username": "morty", "password": "ooh wee"}
-    ]
-
-    try:
-        return jel_assert(
-                problem1_target, 
-                update_username(example, "nothere", "bloop") == False,
-                f"return False if the username was not found"
-            )
-    except NameError as e:
-        return jel_NameError(problem1_target, e)
-    except TypeError as e:
-        return jel_TypeError(problem1_target, e)
-
-# Problem 1.2
-def test_update_username_1():
-
-    example = [
-        { "username": "jesse", "password": None},
-        { "username": "morty", "password": "ooh wee"}
-    ]
-
-    update_username(example, "jesse", "misterpants")
-
-    try:
-        return jel_assert(
-                problem1_target, 
-                example[0]['username'] == "misterpants",
-                f"update the username successfully"
-            )
-    except NameError as e:
-        return jel_NameError(problem1_target, e)
-    except TypeError as e:
-        return jel_TypeError(problem1_target, e)
-
-# Problem 1.2
-def test_update_username_1():
-
-    example = [
-        { "username": "jesse", "password": None},
-        { "username": "morty", "password": "ooh wee"}
-    ]
-
-    update_username(example, "jesse", "misterpants")
-
-    try:
-        return jel_assert(
-                problem1_target, 
-                example[0]['username'] == "misterpants",
-                f"update the username successfully"
-            )
-    except NameError as e:
-        return jel_NameError(problem1_target, e)
-    except TypeError as e:
-        return jel_TypeError(problem1_target, e)
+def test_shark_class_get_health():
     
-# Problem 2
+    test = Shark("Testman", 5)
 
-def test_generate_password_for_user_0():
-
-    problem2_target = "generate_password_for_user()"
-
-    example = [
-        { "username": "jesse", "password": None},
-        { "username": "morty", "password": "ooh wee"}
-    ]
-
-    generate_password_for_user(example, "jesse")
+    # Check if self.health was created
+    try:
+        test.get_health()
+    except AttributeError:
+        return jel_assert(
+            test2_target,
+            False,
+            f"do anything because no class method named `get_health()` was found!"
+        )
 
     try:
         return jel_assert(
-                problem2_target, 
-                example[0]['password'] is not None,
-                f"update the given user's password correctly (was None, now it's '{example[0]['password']}')"
+                test2_target, 
+                test.get_health() == 5,
+                f"return 5 when constructor was passed 5."
             )
     except NameError as e:
-        return jel_NameError(problem2_target, e)
+        return jel_NameError(test2_target, e)
     except TypeError as e:
-        return jel_TypeError(problem2_target, e)
+        return jel_TypeError(test2_target, e)
+
+test3_target = "bite()"
+
+def test_shark_class_bite():
+    
+    test = Shark("Testman", 50)
+    target = Shark("Bitee", 50)
+
+    # Check if self.health was created
+    try:
+        test.bite(target)
+    except AttributeError:
+        return jel_assert(
+            test3_target,
+            False,
+            f"do anything because no class method named `bite()` was found"
+        )
+
+    try:
+        return jel_assert(
+                test3_target, 
+                target.get_health() == 25,
+                f"subtract 25 from target's health (got {target.get_health()})"
+            )
+    except NameError as e:
+        return jel_NameError(test3_target, e)
+    except TypeError as e:
+        return jel_TypeError(test3_target, e)
+
+test4_target = "die()"
+
+def test_shark_class_die():
+    
+    test = Shark("Testman", 50)
+    target = Shark("Bitee", 25)
+
+    # Check if self.health was created
+    try:
+        target.die()
+    except AttributeError:
+        return jel_assert(
+            test4_target,
+            False,
+            f"do anything because no class method named `die()` was found"
+        )
+
+    try:
+        return jel_assert(
+                test4_target, 
+                target.get_health() == 0,
+                f"set health to zero"
+            )
+    except NameError as e:
+        return jel_NameError(test4_target, e)
+    except TypeError as e:
+        return jel_TypeError(test4_target, e)
--- assignments/m12-networkintel1/lawsonjesse_123_123_userauth.py
+++ assignments/m12-networkintel1/lawsonjesse_123_123_userauth.py
@@ -1,151 +1,31 @@
 
-import os
+class Shark:
+
+    # Class variables
+    animal_type = "fish"
+    location = "ocean"
+
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def get_health(self):
+        return self.health
+
+    def bite(self, target):
+        target.health -= 25
+        if target.health < 0:
+            target.die()
+
+    def die(self):
+        self.health = 0
 
 
 
-# The class LogParser will parse a directory of log files in the following format:
-# <timestamp> <ip> <activity> <url>
-
-# For the M13 
-class IPAddress:
-    def __init__(self, ip):
-        self.ip = ip
-        self.activity = []
-        self.activity_url = []
-        self.total_activity = 0
-        self.failed_logins = 0
-        self.suspicious = False
-        # ip = the ip address of the record
-        # failed_logins = The number of failed logins this ip has had
-        # flar = "Failed Login Activity Ratio". (failed_logins) / (total_activity)
-
-    def is_suspicious(self):
-        return self.suspicious
-
-    def set_as_suspicious(self):
-        self.suspicious = True
-
-    def get_ip(self):
-        return self.ip
-
-    def add_activity(self, some_activity, some_url):
-        self.activity.append(some_activity)
-        self.activity_url.append(some_url)
-        
-        # Recalculate total_activity and failed_logins
-        self.total_activity += 1
-        if some_activity == "login_failed":
-            self.failed_logins += 1
-
-    def get_total_activity(self):
-        return self.total_activity
-        #return len(self.activity)
-
-    def get_failed_logins(self):
-        return self.failed_logins
-        #num_failed = 0
-        #for a in self.activity:
-        #    num_failed += 1 if a == "login_failed"
-        #return num_failed
-
-    def get_flar(self):
-        return round( (self.get_failed_logins() / self.get_total_activity()), 2)
-
-
-class LogParser:
-    def __init__(self, filename, sensitivity = 0.75):
-        self.filename = filename
-        self.all_ips = [] # A list of all tracked IPs (IPAddress objects)
-        
-        # The sensitivity is a decimal from 0 to 1, representing the percent 
-        # of total activity that can be failed logins before marking the IP
-        # as suspicious. 
-        self.sensitivity = sensitivity
-        self.verified = False
-        self.complete = False
-
-        # Verify filename exists
-        if os.path.exists(filename):
-            self.verified = True
-
-
-    def parse(self):
-        """
-        Primary function of LogParser objects. This parses self.filename 
-        and extracts any suspicious_ips (according to self.sensitivity), recording 
-        them in the self.suspicious_ips dict.
-        """
-        if not self.verified:
-            print(f"Error: Could not parse {filename}. I don't think it exists.")
-            exit() # Quit the program
-
-        # Open the file and start tracking IPs
-        print("Parsing log file...")
-        with open(self.filename, "r") as the_file:
-            for line in the_file:
-                # line format: <timestamp> <ip> <activity> <url>
-                parts = line.split()
-                the_ip = parts[1]
-                the_activity = parts[2]
-                the_url = parts[3]
-
-                #print(f"Parsing IP {the_ip}:")
-
-                # Assume this is a new IP.
-                a_new_ip = True
-                
-                # Loop through all_ips and if we find it, set a_new_ip to false,
-                # and add the activity to our IP.
-                for ip in self.all_ips:
-                    #print(f"Comparing {ip.get_ip()} == {the_ip}...")
-                    if ip.get_ip() == the_ip:
-                        #print(f"\tNot a new IP!")
-                        a_new_ip = False
-                        ip.add_activity(the_activity, the_url)
-
-                if a_new_ip:
-                    #print(f"\tCreating new object...")
-                    the_ip_object = IPAddress(the_ip)
-                    the_ip_object.add_activity(the_activity, the_url)
-                    self.all_ips.append(the_ip_object)
-                    
-        
-        # Now we're finished parsing each line in the file
-
-        # We now need to flag any IP with flar > sensitivity as suspicious
-
-        # Loop through all_ips and find any whose flar is > sensitivity
-        print("Analyzing suspicious activity...")
-        for ip in self.all_ips:
-            if ip.get_flar() > self.sensitivity:
-                #print(f"Setting {the_ip} as suspicious ({ip.get_flar()})")
-                ip.set_as_suspicious()
-        
-
-    
-
-    def report(self, out_file = "report.txt"):
-        # Writes the suspicious IPs to out_file (a file)
-        # Format: <ip> <flar> (<total_activity>/<num_failed_logins>)
-        
-        f = open("report.txt", "w")
-
-        print("Building report...")
-
-        for ip in self.all_ips:
-            #print(f"report(): Checking IP {ip.get_ip()}... ")
-            if ip.is_suspicious():
-                f.write(f"{ip.get_ip()} {ip.get_flar()} ({ip.get_total_activity()}/{ip.get_failed_logins()})\n")
-        
-        f.close()
 
 def main():
-    
-    parser = LogParser("traffic_logs.txt", 0.35) # Parse file "traffic_logs.txt" with 35% sensitivity
+    shark1 = Shark("Shark1", 50)
+    shark2 = Shark("Shark2", 25)
 
-    parser.parse()
-
-    parser.report("report.txt")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
